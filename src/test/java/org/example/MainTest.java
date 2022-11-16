131a624b9ae1f8136393cd535e132ff331813c57
Fedja Pandzic <fedjap@comtrade.com>
2022-11-16 20:41:34 +0100

    Added tests for Solution class

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -18,10 +18,21 @@ class MainTest {
     }
 
     @Test
-    void throwsIncorrectInputInFileException(){
+    void throwsIncorrectInputInFileExceptionForString(){
         assertThrows(IncorrectInputInFileException.class, ()->{
             String word = "TEST";
             String string = "testTESTTESTTESSSTTTT";
+            Solution solution = new Solution();
+
+            int result = solution.solution(word,string);
+        });
+    }
+
+    @Test
+    void throwsIncorrectInputInFileExceptionForWord(){
+        assertThrows(IncorrectInputInFileException.class, ()->{
+            String word = "TesT";
+            String string = "TESTTESTTESTTESSSTTTT";
             Solution solution = new Solution();
 
             int result = solution.solution(word,string);
